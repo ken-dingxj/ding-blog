f28a7f6870e6634a86d8ff586b2e270a727d787c
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-03-01 21:41:07 +0800

    mod:增加自动部署脚本

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,7 +3,21 @@
 
 set -e
 
-npm istall
+
+docker rmi -f registry.cn-hangzhou.aliyuncs.com/docker_ding/test
+
+npm install
 
 npm run build
 
+GITVER=$(git rev-list HEAD -n 1 | cut -c 1-)
+
+echo ${GITVER}
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER} .
+
+docker push registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+
+docker run -p 80:80 --name test -d registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+
+echo '-------部署成功---------'
