1e7859a38a1f5d346e28d3f719cc0223388650f7
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-03-03 00:55:06 +0800

    mod:修复版本错误

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,14 +3,16 @@
 
 set -e
 
-GITVER=$(git rev-list HEAD^ -n 1 | cut -c 1-)
+GITVER_PRE=$(git rev-list HEAD^ -n 1 | cut -c 1-)
+
+GITVER_CURR=$(git rev-list HEAD -n 1 | cut -c 1-)
 
 docker kill ding
 
 docker rm -f ding 
 
-docker rmi -f registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+docker rmi -f registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER_PRE}
 
-docker run -p 80:80 --name ding -d registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+docker run -p 80:80 --name ding -d registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER_CURR}
 
 echo '-------部署成功---------'
