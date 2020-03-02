8fdb9c919abc24c6213c2055b884e004b7c6af7f
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-03-03 00:28:17 +0800

    修改自动化部署脚本

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,21 +3,14 @@
 
 set -e
 
-
-docker rmi -f registry.cn-hangzhou.aliyuncs.com/docker_ding/test
-
-npm install
-
-npm run build
-
 GITVER=$(git rev-list HEAD -n 1 | cut -c 1-)
 
-echo ${GITVER}
+docker kill ding
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER} .
+docker rm -f ding 
 
-docker push registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+docker rmi -f registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
 
-docker run -p 80:80 --name test -d registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
+docker run -p 80:80 --name ding -d registry.cn-hangzhou.aliyuncs.com/docker_ding/test:${GITVER}
 
 echo '-------部署成功---------'
